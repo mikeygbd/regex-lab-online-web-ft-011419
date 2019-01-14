e5fa751c5cd12af8dd76516bbf013b1b6cd9a7a4
Michael Soares <mikey.soares@gmail.com>
2019-01-14 23:14:25 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,9 +6,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  array=[]
-  array << text.scan(/un*ing/)
-array
+text.scan(/^un.+ing$/)
 end
 
 def words_five_letters_long(text)
