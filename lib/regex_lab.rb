313394b25c0742168acac11e8af9a47a72dbb13a
Michael Soares <mikey.soares@gmail.com>
2019-01-15 00:03:23 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,11 +6,12 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
- text.scan(/\b(un)\b[ing]/)
+ text.scan(/\b(un).+\b(ing)/)
 
 end
 
 def words_five_letters_long(text)
+
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
