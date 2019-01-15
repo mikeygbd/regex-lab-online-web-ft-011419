770899928e30df4e2718383a508fc9813d59b4c9
Michael Soares <mikey.soares@gmail.com>
2019-01-15 01:07:59 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,12 +7,10 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
  text.scan(/\bun[a-z]+ing/)
-
 end
 
 def words_five_letters_long(text)
   text.scan(/\b\w{5}\b/)
-
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
@@ -21,8 +19,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
   else
     return false
   end
-
-
 end
 
 def valid_phone_number?(phone)
@@ -31,5 +27,4 @@ def valid_phone_number?(phone)
   else
     return false
   end
-
 end
